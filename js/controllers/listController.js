00d026c5004c8352734a2a2be59a72bb80e3f3dd
aniket umbc <aniket.umbc@gmail.com>
2018-08-08 08:08:30 +0530

    img add and Json object add bind list

--- js/controllers/listController.js
+++ js/controllers/listController.js
@@ -7,7 +7,70 @@
         var vm;
         vm = this;
         vm.name = "App";
+        vm.data = turtlesData;
 
     }
+
+    var turtlesData = [
+        {
+            type:"Green Turtle",
+            location:"Tropical and subtropical",
+            size:"Upto 1.5m to 300kg",
+            lifespan:"Over 80 years",
+            diet:"veg",
+            desscription:"Turtles are reptiles that have been around for more than 200 million years. In fact, fossils from the dinosaur age show that turtles today look pretty much the same as they did way back then."
+            
+        },
+        {
+            type:"Green Turtle 1",
+            location:"Tropical and subtropical",
+            size:"Upto 1.5m to 300kg",
+            lifespan:"Over 80 years",
+            diet:"veg",
+            desscription:"Turtles are reptiles that have been around for more than 200 million years. In fact, fossils from the dinosaur age show that turtles today look pretty much the same as they did way back then."
+            
+        },
+        {
+            type:"Green Turtle 2",
+            location:"Tropical and subtropical",
+            size:"Upto 1.5m to 300kg",
+            lifespan:"Over 80 years",
+            diet:"veg",
+            desscription:"Turtles are reptiles that have been around for more than 200 million years. In fact, fossils from the dinosaur age show that turtles today look pretty much the same as they did way back then."
+            
+        },{
+            type:"Green Turtle 3",
+            location:"Tropical and subtropical",
+            size:"Upto 1.5m to 300kg",
+            lifespan:"Over 80 years",
+            diet:"veg",
+            desscription:"Turtles are reptiles that have been around for more than 200 million years. In fact, fossils from the dinosaur age show that turtles today look pretty much the same as they did way back then."
+            
+        },{
+            type:"Green Turtle 4",
+            location:"Tropical and subtropical",
+            size:"Upto 1.5m to 300kg",
+            lifespan:"Over 80 years",
+            diet:"veg",
+            desscription:"Turtles are reptiles that have been around for more than 200 million years. In fact, fossils from the dinosaur age show that turtles today look pretty much the same as they did way back then."
+            
+        },{
+            type:"Green Turtle 5",
+            location:"Tropical and subtropical",
+            size:"Upto 1.5m to 300kg",
+            lifespan:"Over 80 years",
+            diet:"veg",
+            desscription:"Turtles are reptiles that have been around for more than 200 million years. In fact, fossils from the dinosaur age show that turtles today look pretty much the same as they did way back then."
+            
+        },{
+            type:"Green Turtle 6",
+            location:"Tropical and subtropical",
+            size:"Upto 1.5m to 300kg",
+            lifespan:"Over 80 years",
+            diet:"veg",
+            desscription:"Turtles are reptiles that have been around for more than 200 million years. In fact, fossils from the dinosaur age show that turtles today look pretty much the same as they did way back then."
+            
+        }
+    ]
 
 })();
