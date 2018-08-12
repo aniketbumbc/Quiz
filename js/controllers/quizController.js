98620e96bfd70eb357c0801a25a53d0ec6a7ccde
aniket umbc <aniket.umbc@gmail.com>
2018-08-12 22:55:12 +0530

    added error message for unanwser question

--- js/controllers/quizController.js
+++ js/controllers/quizController.js
@@ -1,52 +1,72 @@
-(function(){
+(function () {
 
-angular.module("turtleFacts").controller("quizCtrl",QuizController);
+    angular.module("turtleFacts").controller("quizCtrl", QuizController);
 
-QuizController.$inject = ['quizMatricService' ,'DataService'];
+    QuizController.$inject = ['quizMatricService', 'DataService'];
 
-function QuizController(quizMatricService,DataService){
-    var vm = this;
-    vm.quizMatricService = quizMatricService;  // accessing serive in controller 
-    vm.DataService = DataService;
-    vm.activeQuestion = 0; // flag set 
-    vm.questionAnwsered = questionAnwsered;
-    vm.setActiveQuestion = setActiveQuestion;
-    vm.selectAnwser= selectAnwser;
+    function QuizController(quizMatricService, DataService) {
+        var vm = this;
+        vm.quizMatricService = quizMatricService; // accessing serive in controller 
+        vm.DataService = DataService;
+        vm.activeQuestion = 0; // flag set 
+        vm.questionAnwsered = questionAnwsered;
+        vm.setActiveQuestion = setActiveQuestion;
+        vm.selectAnwser = selectAnwser;
+        vm.error = false;  // flag declare for error
+        var numQuestionAnwserd = 0;
+        vm.finalise = false;
 
-    var numQuestionAnwserd = 0;
 
+        function setActiveQuestion(index) {
 
-    function setActiveQuestion(){
-        var breakOut = false;
-        var quizLength = DataService.quizQuestions.length-1;
+            if (index === undefined) {
+                var breakOut = false;
+                var quizLength = DataService.quizQuestions.length - 1;
 
-        while(!breakOut){ 
-            vm.activeQuestion = vm.activeQuestion < quizLength ? ++vm.activeQuestion:0;  // check next question if not next question then 0 
-        if(DataService.quizQuestions[vm.activeQuestion].selected === null){
-            breakOut = true; // found next unanwerd question
+                while (!breakOut) {
+                    vm.activeQuestion = vm.activeQuestion < quizLength ? ++vm.activeQuestion : 0; // check next question if not next question then 0 
+                    
+                    if(vm.activeQuestion === 0){
+                        vm.error = true;
+                    }
+                    
+
+                    if (DataService.quizQuestions[vm.activeQuestion].selected === null) {
+                        breakOut = true; // found next unanwerd question
+                    }
+                }
+            } else {
+                vm.activeQuestion = index;
+            }
+
         }
+
+        function questionAnwsered() {
+            var quizLength = DataService.quizQuestions.length;
+            // alert("hello");
+
+            if (DataService.quizQuestions[vm.activeQuestion].selected !== null) {
+                numQuestionAnwserd++;
+                if (numQuestionAnwserd >= quizLength) {
+                    //finalize quiz
+                    for(var i =0; i<quizLength; i++){
+                        if(DataService.quizQuestions[i].selected === null){
+                            setActiveQuestion(i);
+                            return;
+                        }
+                    }
+                    vm.error = false;
+                    vm.finalise = true;
+                    return;;
+                }
+            }
+            vm.setActiveQuestion();
         }
-      
+
+        function selectAnwser(index) {
+
+            DataService.quizQuestions[vm.activeQuestion].selected = index;
+        }
+
     }
-
-    function questionAnwsered(){
-        var quizLength = DataService.quizQuestions.length;
-       // alert("hello");
-
-        if(DataService.quizQuestions[vm.activeQuestion].selected !== null){
-            numQuestionAnwserd++;
-            if(numQuestionAnwserd >= quizLength){
-                  //finalize quiz
-
-            }           
-          }
-          vm.setActiveQuestion();
-    }
-    
-    function selectAnwser(index){
-
-        DataService.quizQuestions[vm.activeQuestion].selected = index;
-    }
-
-}
 })();
