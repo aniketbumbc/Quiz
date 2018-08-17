93f607c4e2b7dfd015d7f8d56f37d1854b5560cd
aniket umbc <aniket.umbc@gmail.com>
2018-08-17 06:52:11 +0530

    added class for right and wrong question

--- js/controllers/resultController.js
+++ js/controllers/resultController.js
@@ -11,8 +11,28 @@
        debugger;
        vm.name = "aniket";
         vm.quizMatricService = quizMatricService;
-        vm.DataService = DataService;
+        vm.DataService = DataService;       
+        vm.getAnwersClass = getAnwersClass;
+        vm.setActiveQuestion = setActiveQuestion;
+        vm.activeQuestion = 0;
 
+        function setActiveQuestion(index){
+            vm.activeQuestion = index; 
+
+        }
+
+
+
+
+        function  getAnwersClass(index){    // ng- class implemention
+
+            if(index == quizMatricService.correctAnwsers[vm.activeQuestion]){
+                return "bg-success";
+            }else if(index == DataService.quizQuestions[vm.activeQuestion].selected){
+                return "bg-danger";
+                
+            }
+        }
     }
 
 })();
